61c305e150a76482110a0567277a003909f41255
Guilherme C <62406808+guilherme-couto@users.noreply.github.com>
2023-04-03 20:51:05 -0300

    Bug fixes and plots ready

--- plot.py
+++ plot.py
@@ -1,19 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import os
+import imageio.v2
 
-U = np.zeros((50, 200, 200))
+if len(sys.argv) != 6:
+    print('Usage: python3 plot.py <method> <dt_ode> <dt_pde> <total_frames> <frame_rate>')
+    sys.exit(1)
 
+method = sys.argv[1]
+dt_ode = sys.argv[2]
+dt_pde = sys.argv[3]
+totalframes = int(sys.argv[4])
+framerate = int(sys.argv[5])
 
-# Read data from file
-with open('tnnp-ADI-0.020-0.040.txt', 'r') as f:
-    for k in range(len(U)):
+U = np.zeros((totalframes, 200, 200))
+t = np.zeros(totalframes)
+
+filename = f'tnnp-{method}-{dt_ode}-{dt_pde}.txt'
+timesfile = f'sim-times-{method}-{dt_ode}-{dt_pde}.txt'
+
+# Read data from files
+with open(filename, 'r') as f:
+    for n in range(totalframes):
         for i in range(len(U[0])):
             for j in range(len(U[0])):
-                U[k][i][j] = float(f.readline())
+                U[n][i][j] = float(f.readline())
 
-# Fill variable
-plt.imshow(U[5], cmap='hot', vmin=-100, vmax=50)
-plt.colorbar()
-plt.savefig('tnnp-ADI-0.020-0.040.png')
+with open(timesfile, 'r') as f:
+    for n in range(totalframes):
+        t[n] = float(f.readline())
+
+# Make plots
+plots = []
+for n in range(len(U)):
+    if n % framerate == 0:
+        plotname = 'plot-' + str(n) + '.png'
+        plots.append(plotname)
+        
+        plt.imshow(U[n], cmap='plasma', vmin=-100, vmax=50)
+        plt.colorbar(label='V (mV)')
+        plt.title(f'Monodomain TNNP {method} dtode = {dt_ode} dtpde = {dt_pde} t = {t[n]:.2f}')
+        plt.xticks([])
+        plt.yticks([])
+        
+        plt.savefig(plotname)
+        plt.close()
+
+# Build gif
+with imageio.v2.get_writer(f'{method}-{dt_ode}-{dt_pde}.gif', mode='I') as writer:
+    for plot in plots:
+        image = imageio.v2.imread(plot)
+        writer.append_data(image)
+        
+# Remove files
+for png in set(plots):
+    os.remove(png)
+
     
     
